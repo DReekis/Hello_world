260786d62dcc922ba4c5f75aa049c2d2f36ba850
DReek <77190544+DReekis@users.noreply.github.com>
2023-12-23 19:18:15 +0530

    2.

--- new.py
+++ new.py
@@ -1,1 +1,21 @@
 
+def int_to_roman(num):
+    values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+    roman = ""
+    i = 0
+
+    while num > 0:
+        count = num // values[i]
+        num %= values[i]
+
+        while count > 0:
+            roman += symbols[i]
+            count -= 1
+
+        i += 1
+
+    return roman
+
+
